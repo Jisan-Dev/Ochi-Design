702804caddad00eeec916008a60df450cb194702
Jisun696 <istiakkashem35@gmail.com>
2024-02-08 21:25:23 +0600

    Start adding the landing page (hero/banner)

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="w-full h-screen bg-zinc-900 pt-1">
+      <div className="textstructure mt-40 px-20">
+        <div className="masker">
+          <h1 className="uppercase text-9xl font-['Bebas_Neue'] leading-none">We create</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
