93d0ad40934596af3b4bccabdd8ff05bb12c0ea4
Jisun696 <istiakkashem35@gmail.com>
2024-02-09 23:25:52 +0600

    Start adding the marquee part

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Marquee = () => {
+  return (
+    <div className="w-full py-10 bg-[#004D43]">
+      <div className="border-t-2 border-b-2 border-zinc-300 flex">
+        <h1 className="text-8xl">we are ochi</h1>
+        <h1 className="text-8xl">we are ochi</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Marquee;
