a1b90bf45ee959de75f83b530cf1a48482a02489
Jisun696 <istiakkashem35@gmail.com>
2024-02-10 23:30:44 +0600

    Marquee section added animation will be added later

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 const Marquee = () => {
   return (
-    <div className="w-full py-10 bg-[#004D43]">
-      <div className="border-t-2 border-b-2 border-zinc-300 flex">
-        <h1 className="text-8xl">we are ochi</h1>
-        <h1 className="text-8xl">we are ochi</h1>
+    <div className="w-full pt-20 pb-16 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]">
+      <div className="border-t-[1px] border-b-[1px] border-zinc-500 flex gap-10 whitespace-nowrap overflow-hidden">
+        <h1 className="text-[22vw] leading-none font-['Founders_Grotesk_X_Condensed'] uppercase  font-medium -mb-24 pt-4">
+          we are ochi
+        </h1>
+        <h1 className="text-[22vw] leading-none font-['Founders_Grotesk_X_Condensed'] uppercase font-medium -mb-24 pt-4">
+          we are ochi
+        </h1>
       </div>
     </div>
   );
