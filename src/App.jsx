4529ffc75724e9bb7abc24165ca813bc555c590a
Jisun696 <istiakkashem35@gmail.com>
2024-02-12 01:43:08 +0600

    Add/install locomotive scroll package

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ import LandingPage from './components/LandingPage';
 import LogoCards from './components/LogoCards';
 import Marquee from './components/Marquee';
 import Navbar from './components/Navbar';
+import LocomotiveScroll from 'locomotive-scroll';
 
 const App = () => {
+  const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <div className="w-full min-h-screen text-white bg-zinc-900">
       <Navbar />
