7f430c8bc81cc26e6a8a859981e05318d9f1baff
Jisun696 <istiakkashem35@gmail.com>
2024-02-11 22:23:54 +0600

    Start adding the Featured section

---
+++ src/components/Featured.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Featured = () => {
+  return (
+    <div className="w-full py-20">
+      <div className="w-full px-20 border-b-[1px] border-zinc-700 pb-20">
+        <h1 className="text-7xl tracking-tight font-['Neue_Montreal']">Featured projects</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
