525db5cc09674ca966d336f5f9f7cfb1ddc06ce4
Jisun696 <istiakkashem35@gmail.com>
2024-02-11 23:21:34 +0600

    Featured section done without animation and effects

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -6,6 +6,30 @@ const Featured = () => {
       <div className="w-full px-20 border-b-[1px] border-zinc-700 pb-20">
         <h1 className="text-7xl tracking-tight font-['Neue_Montreal']">Featured projects</h1>
       </div>
+      <div className="px-20">
+        <div className="cards w-full flex gap-10 mt-10">
+          <div className="cardcontainer relative w-1/2 h-[75vh]">
+            <h1 className='absolute z-10 left-full -translate-x-1/2 top-1/2 -translate-y-1/2 text-[#CDEA68] font-["Bebas_Neue"] text-8xl leading-none'>
+              {'FYDE'.split('').map((char, index) => (
+                <span key={index}>{char}</span>
+              ))}
+            </h1>
+            <div className="card w-full h-full rounded-xl overflow-hidden">
+              <img className="w-full h-full object-cover" src="../images/Featured1.png" alt="" />
+            </div>
+          </div>
+          <div className="cardcontainer relative w-1/2 h-[75vh]">
+            <h1 className='absolute right-full translate-x-1/2 top-1/2 -translate-y-1/2 text-[#CDEA68] font-["Bebas_Neue"] text-8xl leading-none'>
+              {'VISE'.split('').map((char, index) => (
+                <span key={index}>{char}</span>
+              ))}
+            </h1>
+            <div className="card w-full h-full rounded-xl overflow-hidden">
+              <img className="w-full h-full object-cover" src="../images/Featured2.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
