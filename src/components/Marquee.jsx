432e6c915868b97a332e5fd18eda37490574a875
Jisun696 <istiakkashem35@gmail.com>
2024-02-12 16:11:22 +0600

    marquee animation added

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,15 +1,24 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 
 const Marquee = () => {
   return (
     <div className="w-full pt-20 pb-16 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]">
-      <div className="border-t-[1px] border-b-[1px] border-zinc-300 flex gap-10 whitespace-nowrap overflow-hidden">
-        <h1 className="text-[22vw] leading-none font-['Founders_Grotesk_X_Condensed'] uppercase  font-medium -mb-[7vw] pt-4">
+      <div className="border-t-[1px] border-b-[1px] border-zinc-300 flex  whitespace-nowrap overflow-hidden">
+        <motion.h1
+          initial={{ x: '0' }}
+          animate={{ x: '-100%' }}
+          transition={{ repeat: Infinity, ease: 'linear', duration: 10 }}
+          className="text-[22vw] leading-none font-['Founders_Grotesk_X_Condensed'] uppercase  font-medium -mb-[7vw] pt-4 pl-20">
           we are ochi
-        </h1>
-        <h1 className="text-[22vw] leading-none font-['Founders_Grotesk_X_Condensed'] uppercase font-medium -mb-[7vw] pt-4">
+        </motion.h1>
+        <motion.h1
+          initial={{ x: '0' }}
+          animate={{ x: '-100%' }}
+          transition={{ repeat: Infinity, ease: 'linear', duration: 10 }}
+          className="text-[22vw] leading-none font-['Founders_Grotesk_X_Condensed'] uppercase font-medium -mb-[7vw] pt-4 pl-20">
           we are ochi
-        </h1>
+        </motion.h1>
       </div>
     </div>
   );
